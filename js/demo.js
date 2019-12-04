a26d4815af47ebbf2d53f560735a9e71cf82da30
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-04 12:51:20 +0800

    add try again button

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,10 @@
 let videoName = ''
 const modelList = ['陳昇瑋','韓國瑜','蔡英文'];
 const list = document.getElementById('select_model');
+const uploadForm= document.getElementById("uploadForm");
+const btnAgain=document.getElementById("btnAgain");
+
+
 for (let i = 0; i < modelList.length; i++) {
   const option = document.createElement('option');
   option.innerHTML = modelList[i];
@@ -14,7 +18,10 @@ const addFile = () => {
     const vildeoFile = document.getElementById('videoFile').files;
     const modelIdx = document.getElementById('select_model').value;
     if (vildeoFile.length) {
+      // loading animation
       document.getElementById("loading").classList.remove("d-none");
+      // hidden upload form
+      uploadForm.classList.add("d-none");
       // 上傳 Image 檔案
       const formData = new FormData();
       videoName=`srcVideo.${vildeoFile[0].name.split(".")[1]}`;
@@ -48,7 +55,10 @@ const addFile = () => {
                 autoplay: 'muted',
                 controls: true
               });
+              // Stop loaging animate
               document.getElementById("loading").classList.add("d-none");
+              // Show try again button
+              btnAgain.classList.remove("d-none");
             },
               (error) => {
                 var message = error.response.data.message;
--- js/demo2.js
+++ js/demo2.js
@@ -1,7 +1,23 @@
-const uploadMethod = document.forms["fileForm"].elements["radio_upload"];
+const uploadMethod = document.forms["uploadForm"].elements["radio_upload"];
+const webcamRecord = document.getElementById("webcamRecord");
+const browseFile = document.getElementById("browseFile");
 
-for(radio in uploadMethod) {
-  uploadMethod[radio].onclick = function() {
+for (radio in uploadMethod) {
+  uploadMethod[radio].onchange = function() {
       console.log(this.value);
+      if(this.value==='Browse File'){
+        webcamRecord.classList.add("d-none");
+        browseFile.classList.remove("d-none");
+      }
+      else{
+        webcamRecord.classList.remove("d-none");
+        browseFile.classList.add("d-none");
+      }
   }
+}
+
+
+/** utton Try Again */
+const btntAgain=()=>{
+  window.location.reload();
 }
